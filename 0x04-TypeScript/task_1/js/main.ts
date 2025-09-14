2be1683ec9b5be4375250c66076820ee467c1db0
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-14 12:56:54 +0100

    add readonly to the main.ts file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,8 @@
 // main page 
 
 interface Teacher {
-    firstName: string;
-    lastName: string;
+    readonly firstName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
