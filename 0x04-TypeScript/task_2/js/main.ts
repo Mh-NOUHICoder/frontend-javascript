df6125662348e56bf2b73ce14a2d03815e39b440
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-14 23:11:55 +0100

    Add export

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -151,11 +151,11 @@ function formatEmployee(employee: Director | Teacher): string {
   return "";
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): string {
+export function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   } else {
